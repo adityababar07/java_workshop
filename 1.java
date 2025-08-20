3c9b5c20f2a7b5af940da4725823ac4be781f81d
Aditya Babar <adityababar715@gmail.com>
2025-08-20 05:43:46 +0000

    push

---
+++ 1.java
@@ -0,0 +1,11 @@
+public class data_types{
+    public static void main (String[] args){
+        int a = 3;
+        System.out.println("a = " + a);
+        String name ;
+        Scanner scanner = new Scanner(System.in);
+        name = scanner.nextLine();
+        System.out.println("name = " + name);
+
+    }
+}
